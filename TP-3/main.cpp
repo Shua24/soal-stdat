0f6cd2ac90b8220a47d33e7f265db4688178cfc2
unknown <jo.daniel.simanjuntak@gmail.com>
2023-09-30 12:27:52 +0700

    Perjelas kode

--- TP-3/main.cpp
+++ TP-3/main.cpp
@@ -6,11 +6,14 @@ using namespace std;
 int main()
 {
     int h, r;
+    // inisialisasi struct cone
+    Cone c = {0, 0};
+    // algoritma
     cout << "Masukkan jari-jari kerucut : ";
     cin >> r;
     cout << "Masukkan tinggi kerucut : ";
     cin >> h;
-    Cone c = {r, h};
+    c = {r, h};
     cout << "Luas alas : " << base(c) << endl;
     cout << "Volume kerucut :" << volume(c) << endl;
     return 0;
